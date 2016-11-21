fe7c5b4c888af03913779fe072f2f2233b415c1d
Catalin Covic <catalin.covic@gmail.com>
2016-11-21 11:59:47 -0500

    cleaned up dev environment

--- gulpfile.js
+++ gulpfile.js
@@ -1,122 +1,66 @@
-// gulp modules
-var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	rename = require('gulp-rename'),
-	minifycss = require('gulp-minify-css'),
-	
-	jade = require('gulp-jade')
-	livereload = require('gulp-livereload')
+const gulp = require('gulp');
+const connect = require('gulp-connect');
+const pug = require('gulp-pug');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const clean = require('gulp-clean-css');
+const header = require('gulp-header');
 
-	;
+const package = require('./package.json');
+const banner = `/*! Simple Hint v${package.version} | Copyright (c) 2014 Catalin Covic | https://github.com/catc */\n`;
 
-
-// public folder paths
-var css    = __dirname + '/public/stylesheets',
-	js     = __dirname + '/public/javascripts',
-	fonts  = __dirname + '/public/fonts',
-	images = __dirname + '/public/images';
-
-
-
-// -------------- development --------------
-var dev = {
-	scssInit  : css + '/scss/*.scss', 		// initial scss processing
-	cssDest   : css, 						// css destination
-	scssWatch : css + '/**/*.scss', 		// scss watch
-	cssReload : css + '/*.css', 			// live-reload watch
+const paths = {
+	css: {
+		src: 'src/simple-hint.scss',
+		style: 'docs/css/scss/**/*.scss'
+	}
 };
 
+// dev
+gulp.task('server', function(){
+	connect.server({
+		root: 'docs',
+		port: 3400,
+		livereload: true
+	});
+})
+gulp.task('pug', function(){
+	return gulp.src('docs/index.pug')
+		.pipe(pug({
 
-// compile scss
-gulp.task('scss', function(){
-	gulp.src( dev.scssInit )
+		}))
+		.pipe(gulp.dest('docs/'));
+})
+gulp.task('sass-src', function(){
+	return gulp.src(paths.css.src)
 		.pipe( sass({errLogToConsole: true}) )
-		.pipe( gulp.dest( dev.cssDest) )
-});
+		.pipe( gulp.dest('docs/css') )
+		.pipe( connect.reload() )
+})
+gulp.task('sass-doc-style', function(){
+	return gulp.src('docs/css/scss/style.scss')
+		.pipe( sass({errLogToConsole: true}) )
+		.pipe( gulp.dest('docs/css') )
+		.pipe( connect.reload() )
+})
+gulp.task('watch', ['pug'], function(){
+	// pug
+	gulp.watch('docs/index.pug', ['pug'])
 
-// watch scss
-gulp.task('watch', function(){
-	gulp.watch( dev.scssWatch, ['scss'] );
+	// sass
+	gulp.watch(paths.css.src, ['sass-src'])
+	gulp.watch(paths.css.style, ['sass-doc-style'])
+})
 
-	// watch css
-	livereload.listen();
-	gulp.watch( dev.cssReload ).on('change', livereload.changed);
-});
+gulp.task('dev', ['server', 'watch'])
 
-
-// start express
-gulp.task('express', function(){
-	require('./bin/www');
-});
-
-
-gulp.task('dev', ['scss', 'express', 'watch'] );
-
-
-
-// -------------- dist --------------
-var dist = {
-	dest : __dirname + '/dist/',
-
-	scssInit : css + '/scss/simple-hint.scss',
-	cssInit : css + '/simple-hint.css'
-};
-
-// scss compile
-gulp.task('dist-scss', function(){
-	gulp.src( dist.scssInit )
-		.pipe( gulp.dest( dist.dest + '/scss/' ) )
-		.pipe( sass() )
-		.pipe( gulp.dest( css ) );
-});
-
-// simple-hint css
-gulp.task('dist-css', function(){
-	gulp.src( dist.cssInit )
-		.pipe( gulp.dest( dist.dest ) )
+// prod
+gulp.task('build', function(){
+	return gulp.src(paths.css.src)
+		.pipe( sass({errLogToConsole: true}) )
+		.pipe( header(banner) )
+		.pipe( gulp.dest('dist') )
 		.pipe( rename({ suffix: '.min'}) )
-		.pipe( minifycss() )
-		.pipe( gulp.dest( dist.dest ) );
-
-});
-
-
-gulp.task('dist', ['dist-scss', 'dist-css'], function(){
-	console.log( 'successfully set up dist' );
-});
-
-
-// -------------- github page --------------
-var gh = {
-	dest : __dirname + '/gh-pages/',
-
-	jadeInit : 'views/index.jade',
-
-	get cssDest (){ return this.dest + '/stylesheets/'; },
-	get jsDest (){ return this.dest + '/javascripts/'; },
-	get fontDest (){ return this.dest + '/fonts/'; },
-	get imageDest (){ return this.dest + '/images/'; },
-};
-
-
-// jade to index.html
-gulp.task('gh-jade', function(){
-	gulp.src( gh.jadeInit )
-		.pipe( jade() )
-		.pipe( gulp.dest( gh.dest ) );
-});
-
-// css, js, images
-gulp.task('gh-public', function(){
-	gulp.src([
-			'public/*/*',
-			'!public/stylesheets/scss/'
-		])
-		.pipe( gulp.dest( gh.dest ) )
-	;
-});
-
-
-gulp.task('gh-page', ['gh-jade', 'gh-public'], function(){
-	console.log( 'successfully updated github page' );
-});
+		.pipe( clean() )
+		.pipe( gulp.dest('dist') )
+})
